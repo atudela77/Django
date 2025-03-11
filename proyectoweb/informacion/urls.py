9da2867102526edf34e1e44443065f078e7825ed
atudela77 <tudehilt@gmail.com>
2025-03-11 20:53:25 +0100

    Martes 11 de marzo

--- proyectoweb/informacion/urls.py
+++ proyectoweb/informacion/urls.py
@@ -5,5 +5,8 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('pelis/', views.pelis, name='pelis'),
     path('futbol/', views.futbol, name='futbol'),
-    path('jugadores/', views.jugadores, name='jugadores')
+    path('jugadores/', views.jugadores, name='jugadores'),
+    path('colores/', views.colores, name='colores'),
+    path('saludo/', views.saludo, name='saludo'),
+    path('sumarnumeros/', views.sumarnumeros, name='sumarnumeros')
 ]
